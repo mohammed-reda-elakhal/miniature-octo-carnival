43161e9f27707af1f48e70f9420f23f8b30accc6
MOHAMMED REDA ELAKHAL <117114078+mohammed-reda-elakhal@users.noreply.github.com>
2022-10-31 20:20:03 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nb = 0;
+            do
+            {
+                Console.WriteLine("entrez un nombre");
+
+            } while (!int.TryParse(Console.ReadLine(), out nb));
+
+            if (premier(nb))
+            {
+                Console.WriteLine("votre nombre est premier");
+            }
+            else
+            {
+                Console.WriteLine("votre nombre pas premier");
+            }
+        }
+        public static bool premier(int a)
+        {
+            if (a < 2)
+            {
+                return true;
+            }
+            else
+            {
+                for (int i = 2; i < a; i++)
+                {
+                    if (a % i == 0)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+    }
+}
