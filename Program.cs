0f4b52077d6d25d1c9bcdf5b1589e0d799e10c50
MOHAMMED REDA ELAKHAL <117114078+mohammed-reda-elakhal@users.noreply.github.com>
2022-11-24 14:26:54 +0100

    Calculatrice en c#

--- Program.cs
+++ Program.cs
@@ -1,45 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-public class Exercise13
+namespace SimpleCalculatrice
 {
-    static int position = 0;
-    public static void Main()
+    internal static class Program
     {
-        int[] tab = new int[10];
-        int i, n,en;
-        Console.Write("\n\nInsert New value in the sorted array :\n");
-        Console.Write("-----------------------------------------\n");
-
-        Console.Write("enter la longueur de tableau : ");
-        n = Convert.ToInt32(Console.ReadLine());
-        Console.Write("Input {0} elements in the array in ascending order:\n", n);
-        for (i = 0; i < n; i++)
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
-            Console.Write("element - {0} : ", i);
-            tab[i] = Convert.ToInt32(Console.ReadLine());
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Calculatrice());
         }
-        Console.Write("Input the value to be inserted : ");
-        en = Convert.ToInt32(Console.ReadLine());
-        Console.Write("The exist array list is :\n ");
-        for (i = 0; i < n; i++)
-            Console.Write("{0} ", tab[i]);
-        for (i = 0; i < n; i++)
-            if (en < tab[i])
-            {
-                position = i;
-                break;
-            }
-        for (i = n; i >= position; i--)
-            tab[i] = tab[i - 1];
-        tab[position] = en;
-
-        Console.Write("\nAfter Insert the list is :\n ");
-        for (i = 0; i <= n; i++)
-            Console.Write("{0} ", tab[i]);
-        Console.Write("\n");
     }
 }
