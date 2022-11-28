35d1ebc2bf613826dd5d0f1276655a282e1b11c9
MOHAMMED REDA ELAKHAL <117114078+mohammed-reda-elakhal@users.noreply.github.com>
2022-11-28 09:56:34 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,46 +1,43 @@
 
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
         static void Main()
-        { int[] arr1 = new int[10];
-    int i,n,p=0,inval;
-       Console.Write("\n\nInsert New value in the sorted array :\n");
-       Console.Write("-----------------------------------------\n");
+        {
+            int[] arr1 = new int[10];
+            int i, n, p = 0, position;
+            Console.Write("-----------------------------------------\n");
 
-    Console.Write("Input the size of array : ");
-     n = Convert.ToInt32(Console.ReadLine()); 
-/* Stored values into the array*/
-       Console.Write("Input {0} elements in the array in ascending order:\n",n);
-       for(i=0;i<n;i++)
-            {
-        Console.Write("element - {0} : ",i);
-     arr1[i] = Convert.ToInt32(Console.ReadLine()); 
-      }
-   Console.Write("Input the value to be inserted : ");
-     inval = Convert.ToInt32(Console.ReadLine());  
-   Console.Write("The exist array list is :\n ");
-   for(i=0;i<n;i++)
-      Console.Write("{0} ",arr1[i]);   
-   /* Determine the position where the new value will be insert.*/
-   for(i=0;i<n;i++)
-     if(inval<arr1[i])
-     {
-       p = i;
-       break;
-     }
-   /* move all data at right side of the array */
-   for(i=n;i>=p;i--)
-      arr1[i]= arr1[i-1];
-   /* insert value at the proper position */
-      arr1[p]=inval;
-   
-      Console.Write("\n\nAfter Insert the list is :\n ");
-   for(i=0;i<=n;i++)
-      Console.Write("{0} ",arr1[i]);
-    Console.Write("\n");
+            Console.Write("entrer le nombre d'element de table : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            /* Stored values into the array*/
+            Console.Write("entrer {0} elements avec une croissante order:\n", n);
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("element {0} : ", i);
+                arr1[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.Write("entrer la valeur que tu veux insérer : ");
+            position = Convert.ToInt32(Console.ReadLine());
+            Console.Write("votre liste est :\n ");
+            for (i = 0; i < n; i++)
+                Console.Write("{0} ", arr1[i]);
+            /* Determine the position where the new value will be insert.*/
+            for (i = 0; i < n; i++)
+                if (position < arr1[i])
+                {
+                    p = i;
+                    break;
+                }
+            /* move all data at right side of the array */
+            for (i = n; i >= p; i--)
+                arr1[i] = arr1[i - 1];
+            /* insert value at the proper position */
+            arr1[p] = position;
+
+            Console.Write("\n\nAprés l'insertion la liste est :\n ");
+            for (i = 0; i <= n; i++)
+                Console.Write("{0} ", arr1[i]);
+            Console.Write("\n");
+
         }
     }
